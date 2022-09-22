e5c663610e10c846533787d7063110c49687e263
junaidbkibria <junaidbkibria@gmail.com>
2022-09-22 16:02:02 +0600

    22 4.01

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,10 @@ console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "production") {
     console.log("inside");
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "../", "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
