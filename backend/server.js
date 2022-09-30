f77989c28d94c4030a7bbf597f70c573150accc7
junaidbkibria <junaidbkibria@gmail.com>
2022-10-01 03:02:46 +0600

    make it better

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "production") {
     console.log("inside");
-  app.use(express.static(path.join(__dirname1, "frontend/build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
