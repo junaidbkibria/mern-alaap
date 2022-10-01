aa9c994e4749b8fd8e87a74ee338ae658bc04a4f
junaidbkibria <junaidbkibria@gmail.com>
2022-10-01 19:20:07 +0600

    pushed backend only

--- backend/server.js
+++ backend/server.js
@@ -28,28 +28,28 @@ app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 
 
-// app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
+app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
-console.log(process.env.NODE_ENV);
+// const __dirname1 = path.resolve();
+// console.log(process.env.NODE_ENV);
 
-if (process.env.NODE_ENV === "production") {
-    console.log("inside");
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//     console.log("inside");
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
 // --------------------------deployment------------------------------
 
